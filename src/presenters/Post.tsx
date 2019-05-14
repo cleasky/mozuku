4df53f499020fefba6ea46ea112a1aa02197563e
96a86c <96a86c@googlegroups.com>
2019-05-14 20:55:36 +0900

    feat: add mock icon

--- src/presenters/Post.tsx
+++ src/presenters/Post.tsx
@@ -4,6 +4,7 @@ import moment from 'moment-timezone'
 
 import OGP from './OGP'
 import Picture from './Picture'
+import Identicon from 'react-identicons'
 
 import {
   Post,
@@ -16,50 +17,63 @@ export default ({ post }: { post: Post }) => {
   return (
     <Twemoji>
       <div className="post">
-        <div className="post__head post-head">
-          <div className="post-head__name">
-            <span className="post-head__name__name">
-              {[].filter
-                .call(
-                  post.author.name.trim(),
-                  (c: string) => c.charCodeAt(0) !== 8203
-                )
-                .join('')
-                .replace(/[\u200B-\u200D\uFEFF]/g, '')
-                .replace(/[\uD800-\uDFFF]{2}/g, '').length
-                ? post.author.name
-                : `@${post.author.screenName}`}
-            </span>
-            <span className="post-head__name__screenName">
-              @{post.author.screenName}
-            </span>
-          </div>
-          <div className="post-head__time">
-            {moment(post.createdAt)
-              .tz('Asia/Tokyo')
-              .format('HH:mm:ss・MM/DD')}
-          </div>
+        <div className="post-icon">
+          <Identicon string={post.author.screenName} size={50} />
         </div>
-        <div className="post__body">
-          {post.body.parts.map((p, i) => {
-            switch (p.type) {
-              case BODYPART_TYPE_LINK:
-              case BODYPART_TYPE_LINK_IMAGE:
-                return (
-                  <a key={i} href={p.payload} target="_blank" rel="noreferrer">
-                    {decodeURI(p.payload)}
-                  </a>
-                )
-              case BODYPART_TYPE_BOLD:
-                return (
-                  <span key={i} className="post__body__bold">
-                    {p.payload}
-                  </span>
-                )
-              default:
-                return <React.Fragment key={i}>{p.payload}</React.Fragment>
-            }
-          })}
+        <div className="post-main">
+          <div className="post__head post-head">
+            <div className="post-head__name">
+              <span className="post-head__name__name">
+                {[].filter
+                  .call(
+                    post.author.name.trim(),
+                    (c: string) => c.charCodeAt(0) !== 8203
+                  )
+                  .join('')
+                  .replace(/[\u200B-\u200D\uFEFF]/g, '')
+                  .replace(/[\uD800-\uDFFF]{2}/g, '').length
+                  ? post.author.name
+                  : `@${post.author.screenName}`}
+              </span>
+              <span className="post-head__name__screenName">
+                @{post.author.screenName}
+              </span>
+            </div>
+            <div className="post-head__time">
+              {moment(post.createdAt)
+                .tz('Asia/Tokyo')
+                .format('HH:mm:ss・MM/DD')}
+            </div>
+          </div>
+          <div className="post__body">
+            {post.body.parts.map((p, i) => {
+              switch (p.type) {
+                case BODYPART_TYPE_LINK:
+                case BODYPART_TYPE_LINK_IMAGE:
+                  return (
+                    <a
+                      key={i}
+                      href={p.payload}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {decodeURI(p.payload)}
+                    </a>
+                  )
+                case BODYPART_TYPE_BOLD:
+                  return (
+                    <span key={i} className="post__body__bold">
+                      {p.payload}
+                    </span>
+                  )
+                default:
+                  return <React.Fragment key={i}>{p.payload}</React.Fragment>
+              }
+            })}
+          </div>
+          <div className="post__meta">
+            via {post.application.name} ({post.application.id})
+          </div>
         </div>
         {post.body.parts
           .filter(p => p.type == BODYPART_TYPE_LINK)
@@ -87,9 +101,6 @@ export default ({ post }: { post: Post }) => {
               </div>
             </React.Fragment>
           ))}
-        </div>
-        <div className="post__meta">
-          via {post.application.name} ({post.application.id})
         </div>
       </div>
     </Twemoji>
